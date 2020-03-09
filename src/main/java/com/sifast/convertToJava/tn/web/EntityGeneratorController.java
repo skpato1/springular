9c924c325a7bbfd8ca6747d7a13f4d9a2829c191
iskander loussaief <iskanderloussaief@gmail.com>
2020-03-09 16:08:42 +0100

    feature/26219

--- src/main/java/com/sifast/convertToJava/tn/web/EntityGeneratorController.java
+++ src/main/java/com/sifast/convertToJava/tn/web/EntityGeneratorController.java
@@ -1,7 +1,5 @@
 package com.sifast.convertToJava.tn.web;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,32 +16,10 @@ public class EntityGeneratorController {
 
 	@Autowired
 	EntityGeneratorService entityGeneratorService;
-
-	@GetMapping("automatic/{packageName}/{className}/{projectName}/{fieldName}/{fieldType}")
-	public void generateFileDynamic(@PathVariable String packageName, @PathVariable String className,
-			@PathVariable String projectName, @PathVariable String fieldName, @PathVariable String fieldType) {
-		entityGeneratorService.generateEntityDynamic(packageName, className, projectName, fieldName, fieldType);
-		System.out.println("file Dynamic generated");
-	}
-
-	@GetMapping("replace/{packageName}/{projectName}")
-	public void replaceString(@PathVariable String packageName, @PathVariable String projectName) throws IOException {
-		entityGeneratorService.replaceString(packageName, projectName);
-		System.out.println("package replaced");
-
-	}
-
-	@GetMapping("array/{packageName}/{className}/{projectName}/{fieldName}/{fieldType}")
-	public void generateFileDynamicWithArray(@PathVariable String packageName, @PathVariable String className,
-			@PathVariable String projectName, @PathVariable String fieldName[], @PathVariable String fieldType[]) {
-		entityGeneratorService.generateEntityDynamicWithArray(packageName, className, projectName, fieldName,
-				fieldType);
-		System.out.println("file Dynamic generated with array");
-	}
+	
 	@GetMapping("model/{className}/{fieldName}/{fieldType}")
 	public void generateFileDynamicWithArrayAngular(@PathVariable String className, @PathVariable String fieldName[], @PathVariable String fieldType[]) {
 		entityGeneratorService.generateEntityDynamicWithArrayAngular(className, fieldName,fieldType);
-		System.out.println("file Dynamic generated with array Angular");
 	}
 
 }

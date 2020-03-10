d2998a34abe487fc90b76def1058bc8c0d5ea36f
iskander loussaief <iskanderloussaief@gmail.com>
2020-03-10 10:47:46 +0100

    feature/26222 add entity attrubute with his dao and timestamp

---
+++ src/main/java/com/sifast/convertToJava/tn/dao/IAttributeDao.java
@@ -0,0 +1,10 @@
+package com.sifast.convertToJava.tn.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.sifast.convertToJava.tn.entities.Attribute;
+
+@Repository
+public interface IAttributeDao extends IGenericDao<Attribute, Integer> {
+
+}
